24955ef1a6a7c6341fdfcd5993c6c33fbd83d858
Ermiyas-Aynetaw <87368573+Ermiyas-Aynetaw@users.noreply.github.com>
2023-09-02 12:09:47 +0300

    email cancelled

--- hcamProjects/settings.py
+++ hcamProjects/settings.py
@@ -127,8 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eromitempo@gmail.com'
-EMAIL_HOST_PASSWORD = 'uzhlwhnskckvhhzn'
+
 
 
 # Static files (CSS, JavaScript, Images)
